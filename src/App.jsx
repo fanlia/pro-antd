db307fbc016830a1d37a4429a8f2760dce05b410
fanlia <you@example.com>
2022-08-01 16:16:49 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { ProLayout } from '@ant-design/pro-components'
 import { UserOutlined } from '@ant-design/icons'
 import { Avatar, Result } from 'antd'
+import logo from './assets/react.svg'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <div id='pro' style={{height: '100vh'}}>
         <ProLayout
             title='Demo'
+            logo={logo}
             rightContentRender={() => {
                 return <Avatar icon={<UserOutlined />} />
             }}
