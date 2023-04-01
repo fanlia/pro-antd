5ef27e4e9af51d184d47866f9934ef9ae600fb11
fanlia <you@example.com>
2023-04-01 16:34:02 +0800

    first commit

--- src/404.jsx
+++ src/404.jsx
@@ -1,16 +1,6 @@
 
-import { Button, Result } from 'antd'
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 export default () => {
-  const navigate = useNavigate()
-
-  return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
-    />
-  )
+  return <Navigate to='/' />
 }
